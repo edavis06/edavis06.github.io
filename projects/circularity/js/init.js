7a1f58d19a26878ecd246650f4a5be92d868ff3d
edavis06 <ed210774@gmail.com>
2021-12-16 23:14:40 +0000

    reseting code

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,20 +20,13 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle;
-        var circles = [];
+
 
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle (){
-            circle = draw.randomCicleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas, 10, 10);
-            view.addChild(circle);
-            circles.push(circle)
-        };
+        
+
         // TODO 3 / 8 : Call the drawCircle() function 
-       for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
-           drawCircle();
-       }
+
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -49,13 +42,10 @@ var init = function (window) {
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-
+           
 
             // TODO 9 : Iterate over the array
-           for (var i = 0; i < circles.length; i++) {
-            physikz.updatePosition(circles[i]);
-            game.checkCirclePosition(circles[i]);
-           }
+           
             
         }
     
@@ -67,20 +57,12 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width) {
+            if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            if ( circle.x < 0) {
-                circle.x = canvas.width;
-            }
-            if ( circle.y > canvas.height) {
-                circle.y = 0;
-            }
-            if ( circle.y < 0) {
-                circle.y = canvas.height;
-            }
+            
 
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
