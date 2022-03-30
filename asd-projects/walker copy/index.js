b26ef925bc8bddc45f7e69b50a11308169f1b876
edavis06 <ed210774@gmail.com>
2022-03-30 00:00:56 +0000

    adding unfinished pong planning

---
+++ asd-projects/walker copy/index.js
@@ -0,0 +1,107 @@
+/* global $, sessionStorage */
+
+$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
+  
+function runProgram(){
+  ////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////// SETUP /////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // Constant Variables
+  var FRAME_RATE = 60;
+  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  var KEY = {
+    "ENTER": 13,
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40,
+    "a": 65,
+    "d": 68,
+    "s": 83,
+    "w": 87
+  }
+  // Game Item Objects
+  var position1X = 0; // the x-coordinate location for the player 1
+  var position1Y = 0; // the y-coordinate location for the player 1
+  var position2X = 0; // the x-coordinate location for the player 2
+  var position2Y = 0; // the y-coordinate location for the player 2
+  var speedX = 0; // the speed for the box along the x-axis
+  var speedY = 0; // the speed for the box along the y-axis
+  var $insTextBox = $("#insTextBox"); //
+  var $tagBox = $("#tagBox");
+  var $simonBox = $("#simonBox");
+  var $twoSimonBox = $("#twoSimonBox");
+
+  // one-time setup
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
+  $($insTextBox).on('click', testButton);
+  $($tagBox).on('click', testButton);
+  $($simonBox).on('click', testButton);
+  $($twoSimonBox).on('click', testButton);
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /* 
+  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+  by calling this function and executing the code inside.
+  */
+  function newFrame() {
+    repositionGameItem();
+    redrawGameItem();
+
+  }
+  
+  function testButton(event){
+    console.log("success");
+  }
+  /* 
+  Called in response to events.
+  */
+  function handleKeyDown(event) {
+    if (event.which === KEY.ENTER){
+      console.log("enter pressed");
+    } else if (event.which === KEY.UP) {
+      speedY = -5;
+      console.log("up pressed");
+    } else if (event.which === KEY.DOWN) {
+      speedY = 5;
+      console.log("down pressed");
+    } else if (event.which === KEY.LEFT) {
+      speedX = -5;
+      console.log("left pressed");
+    } else if (event.which === KEY.RIGHT) {
+      speedX = 5;
+      console.log("right pressed");
+    }
+  }
+  function handleKeyUp(event) {
+    speedX = 0;
+    speedY = 0;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+  function repositionGameItem() {
+    positionX += speedX;
+    positionY += speedY;
+  }
+
+  function redrawGameItem(){
+    $("#walker").css("left", positionX); // draw the box in the new location, positionX pixels away from the "left"
+    $("#walker").css("top", positionY); // draw the box in the new location, positionX pixels away from the "top"
+  }
+
+  function endGame() {
+    // stop the interval timer
+    clearInterval(interval);
+
+    // turn off event handlers
+    $(document).off();
+  }
+  
+}
